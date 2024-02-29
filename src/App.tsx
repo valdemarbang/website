dacaeae743d74b5a7a188aa52aafa64f2b898e9c
Jacalz <Jacalz@users.noreply.github.com>
2024-02-29 13:16:25 +0100

    Update website with new BLHub logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import Button from '@mui/material/Button';
+import logo from './assets/blhub-logo.svg';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return <Button variant="contained" onClick={() => setCount(count + 1)}>Counter: {count}</Button>;
+  return <>
+    <img src={logo} />
+    <Button variant="contained" onClick={() => setCount(count + 1)}>Counter: {count}</Button>
+  </>;
 }
 
 export default App
