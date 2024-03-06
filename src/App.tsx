47555fcbebda39fc2c2e48339358143f04755a68
Valdemar <valdemarbang@hotmail.com>
2024-03-06 16:59:42 +0100

    fixed lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import Button from "@mui/material/Button";
-import logo from "./assets/blhub-logo.svg";
 import TopBar from "./components/TopBar.tsx";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <TopBar />
