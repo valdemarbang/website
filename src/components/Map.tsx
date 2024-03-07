c4261591ccbc30a4c6eb49e70475b533fc6bdf5d
Jacob <jacalz@tutanota.com>
2024-03-07 18:41:03 +0100

    Make sure that the map size updates with window resize

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,17 +1,30 @@
-import { useState, useRef } from "react"
+import { useState, useEffect } from "react"
 import { Map, Marker } from "pigeon-maps"
+
+function getWindowSize(): [number, number] {
+  const {innerWidth, innerHeight} = window;
+  return [innerWidth, innerHeight];
+}
 
 export default function MyMap() {
   const [hue, setHue] = useState(0)
   const color = `hsl(${hue % 360}deg 39% 70%)`
 
-  const height = useRef(window.innerHeight);
-  const width = useRef(window.innerWidth);
-
   const linkoping: [number, number] = [58.4, 15.625278];
 
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+
+  const handleWindowResize = () => {
+      setWindowSize(getWindowSize());
+  } 
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowResize);
+    return () => { window.removeEventListener('resize', handleWindowResize) };
+  }, []);
+
   return (
-    <Map height={height.current} width={width.current}
+    <Map width={windowSize[0]} height={windowSize[1]-65}
 
     defaultCenter={linkoping} defaultZoom={11}>
       <Marker 
@@ -25,9 +38,7 @@ export default function MyMap() {
         anchor={[58.45, 15.6]}
         color={color} 
         onClick={() => setHue(hue + 20)} 
-      >
-
-      </Marker>
+      />
     </Map>
   )
 }
