1d103a6c5d33cdf1b732a414fb5e70f6494798ab
Valdemar <valdemarbang@hotmail.com>
2024-04-04 12:04:56 +0200

    React routes is now implemented in the website

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
+/*
+  Main entry point of the application.
+  It renders the App component into the root element.
+*/
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ src/pages/root.tsx
@@ -0,0 +1,16 @@
+/* 
+    Every page has the header, footer, sidebars, etc. 
+    We don't need to include those files are in the all pages.
+    Instead we can make a parent component all those pages.
+*/
+import TopBar from "../components/TopBar.tsx";
+
+function RootPage() {
+  return (
+    <TopBar />
+    //sidebar...
+    //footer?
+  );
+}
+
+export default RootPage;
