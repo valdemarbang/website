e24e3a376313e0436726a986c63783b5e65887b7
Valdemar <valdemarbang@hotmail.com>
2024-04-09 15:28:40 +0200

    Added a login page and component

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/home.tsx";
+import LoginPage from "../pages/login.tsx";
 import RegisterPage from "../pages/register.tsx";
 
 function RoutesHandler() {
@@ -7,6 +8,7 @@ function RoutesHandler() {
     <BrowserRouter>
       <Routes>
         <Route path="/website" element={<HomePage />} />
+        <Route path="/website/login" element={<LoginPage />} />
         <Route path="/website/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
